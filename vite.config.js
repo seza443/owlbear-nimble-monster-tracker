a80615977d8b58c1714e30a94e169938f36639c3
seza443 <tanguy.vaessen@gmail.com>
2026-02-15 22:14:16 +0100

    ci: get rid of Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,4 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: "/owlbear-nimble-monster-tracker/",
-  server: {
-    cors: {
-      origin: 'https://www.owlbear.rodeo', // Allow requests from this origin
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed methods
-      allowedHeaders: ['Content-Type'], // Specify allowed headers
-    }
-  }
 })
